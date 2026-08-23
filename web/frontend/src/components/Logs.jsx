40713ae292909992743ac1b655dc009803657406
Kiran S <6carbon12.oss@proton.me>
2026-08-23 11:39:17 +0530

    [ftend] Integrate SSE for logs

--- web/frontend/src/components/Logs.jsx
+++ web/frontend/src/components/Logs.jsx
@@ -11,7 +11,6 @@ async function getLogs() {
 }
 
 export default function Logs() {
-  // Mock data representing standard system and network events
   const [logs, setLogs] = useState([]);
 
   useEffect(() => {
@@ -21,19 +20,27 @@ export default function Logs() {
     };
 
     loadLogs();
+    const stream = new EventSource("/api/minecraft/logs/stream", {
+      withCredentials: true,
+    });
+    stream.addEventListener("log", (event) => {
+      const log = JSON.parse(event.data);
+      setLogs((p) => [...p, log]);
+    });
+
+    return () => stream.close();
   }, []);
 
-  // Color mapping for different log severities
   const levelColors = {
-    INFO: "text-tokyo-fg", // Green
-    WARN: "text-tokyo-warning", // Yellow
-    ERROR: "text-tokyo-error", // Red
+    INFO: "text-tokyo-fg",
+    WARN: "text-tokyo-warning",
+    ERROR: "text-tokyo-error",
   };
 
   return (
-    <div className="border-tokyo-border bg-tokyo-surface flex flex-1 w-full max-w-480 flex-col overflow-hidden rounded-md border font-mono text-sm">
+    <div className="border-tokyo-border bg-tokyo-surface flex w-full max-w-480 flex-1 flex-col overflow-hidden rounded-md border font-mono text-sm">
       {/* Scrollable Log Container */}
-      <div className="text-tokyo-fg text-xs h-full space-y-1 overflow-y-auto p-1">
+      <div className="text-tokyo-fg h-full space-y-1 overflow-y-auto p-1 text-xs">
         {logs.toReversed().map((log) => {
           const dateStr = log.time.substring(0, 10);
           const timeStr = log.time.substring(11, 19);
@@ -41,14 +48,16 @@ export default function Logs() {
           return (
             <div
               key={log.id}
-              className="flex gap-2 rounded sm:px-2 sm:py-1 px-1 py-0.5 transition-colors"
+              className="flex gap-2 rounded px-1 py-0.5 transition-colors sm:px-2 sm:py-1"
             >
-              <span className="shrink-0 text-[#565f89] mr-1">
+              <span className="mr-1 shrink-0 text-[#565f89]">
                 <span className="hidden sm:inline">{dateStr}</span>
                 <span className="sm:ml-2">{timeStr}</span>
                 <span className="hidden md:inline">{msStr}</span>
               </span>
-              <span className={"wrap-anywhere " + levelColors[log.level]}>{log.message}</span>
+              <span className={"wrap-anywhere " + levelColors[log.level]}>
+                {log.message}
+              </span>
             </div>
           );
         })}
